cf79852ad0cf75b00a585713bf2c821ac0fb58ae
PENTICTON\cmackay <cmackay@windwardsoftware.com>
2018-10-05 17:13:40 -0700

    Simple Decision Tree Test working

--- SomeDemos/DecisionTree/DecisionTree.cs
+++ SomeDemos/DecisionTree/DecisionTree.cs
@@ -3,24 +3,45 @@ using System.Collections.Generic;
 
 namespace libDecisionTree
 {
-    public class DecisionTree : SampleNode
-    {
-        
-    }
 
     public class Node<T>
     {
         public T Data { get; set; }
-        public Predicate<T> Condition()
+        public Predicate<T> Condition { get; set; }
+        
+        public Node(T Data)
         {
-            return ((T data) => { return true; });
+            this.Data = Data;
         }
 
-        public HashSet<Node<T>> Children {get; set;}
+        //public HashSet<Node<T>> Children {get; set;}
+        public Node<T> TrueChild { get; set; }
+        public Node<T> FalseChild { get; set; }
+        public bool Eval()
+        {
+            if (Condition(Data))
+            {
+                return (TrueChild == null) ? true : TrueChild.Eval();
+            }
+            else
+            {
+                return (FalseChild == null) ? false : FalseChild.Eval();
+            }
+        }
+    }
+
+    public class NotZeroIsTrue : Node<int>
+    {
+        public NotZeroIsTrue(int Data) : base(Data)
+        {
+            this.Condition = (x) => { return x != 0; };
+        }
     }
 
     public class SampleNode : Node<int>
     {
-        new public HashSet<SampleNode> Children { get; set; }
+        public SampleNode(int Data) : base(Data)
+        {
+        }
     }
 }
--- SomeDemos/TestDecisionTree/UnitTestDecisionTree.cs
+++ SomeDemos/TestDecisionTree/UnitTestDecisionTree.cs
@@ -1,5 +1,6 @@
 using libDecisionTree;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 
 
@@ -11,11 +12,95 @@ namespace TestDecisionTree
         [TestMethod]
         public void SimpleTreeCreation()
         {
-            SampleNode N = new SampleNode();
-            N.Data = 0;
-            Assert.IsTrue(ExecutePredicate<int>.Exec(N.Condition(), 0));
+            SampleNode N = new SampleNode(0);
+            N.Condition = (x) => { return true; };
+            Assert.IsTrue(ExecutePredicate<int>.Exec(N.Condition, 0));
         }
 
+        [TestMethod]
+        public void NotZeroIsTrue_NodeTest()
+        {
+            NotZeroIsTrue N2 = new NotZeroIsTrue(0);
+            Assert.IsFalse(ExecutePredicate<int>.Exec(N2.Condition, N2.Data));
+            Assert.IsTrue(ExecutePredicate<int>.Exec(N2.Condition, 1));
+
+        }
+
+        [TestMethod]
+        public void ModelANestedIf_AsAndMatrix()
+        {
+            //      | true | false
+            //---------------------
+            // true | true | false
+            // false| false| false
+
+            List<bool> lstIf = new List<bool>();
+            List<bool> lstTree = new List<bool>();
+
+            Tuple<bool, bool> TT = new Tuple<bool, bool>(true, true);
+            Tuple<bool, bool> TF = new Tuple<bool, bool>(true, false);
+            Tuple<bool, bool> FT = new Tuple<bool, bool>(false, true);
+            Tuple<bool, bool> FF = new Tuple<bool, bool>(false, false);
+
+            Predicate<Tuple<bool, bool>> IfVersionOfAnd = (Tuple<bool, bool> x) =>
+            {
+                if (x.Item1)
+                {
+                    if (x.Item2)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (x.Item2)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                };
+            };
+
+            lstIf.Add(IfVersionOfAnd(TT));
+            lstIf.Add(IfVersionOfAnd(TF));
+            lstIf.Add(IfVersionOfAnd(FT));
+            lstIf.Add(IfVersionOfAnd(FF));
+
+            // build up the same thing as an expression tree
+            Node<Tuple<bool, bool>> T = new Node<Tuple<bool, bool>>(TT);
+            T.Condition = (Tuple<bool, bool> x) => { return x.Item1; };
+            Node<Tuple<bool, bool>> TrueChild = new Node<Tuple<bool, bool>>(TT);
+            TrueChild.Condition = (Tuple<bool, bool> x) => { return x.Item2; };
+            TrueChild.Data = T.Data;
+            Node<Tuple<bool, bool>> FalseChild = new Node<Tuple<bool, bool>>(TT);
+            FalseChild.Condition = (Tuple<bool, bool> x) => { return false; };
+            FalseChild.Data = T.Data;
+
+            T.TrueChild = TrueChild;
+            T.FalseChild = FalseChild;
+
+            T.Data = TT; TrueChild.Data = TT; FalseChild.Data = TT;
+            lstTree.Add(T.Eval());
+            T.Data = TF; TrueChild.Data = TF; FalseChild.Data = TF;
+            lstTree.Add(T.Eval());
+            T.Data = FT; TrueChild.Data = FT; FalseChild.Data = FT;
+            lstTree.Add(T.Eval());
+            T.Data = FF; TrueChild.Data = FF; FalseChild.Data = FF;
+            lstTree.Add(T.Eval());
+
+            Assert.AreEqual(lstIf[0], lstTree[0]);
+            Assert.AreEqual(lstIf[1], lstTree[1]);
+            Assert.AreEqual(lstIf[2], lstTree[2]);
+            Assert.AreEqual(lstIf[3], lstTree[3]);
+
+        }
     }
 
     public static class ExecutePredicate<T>
