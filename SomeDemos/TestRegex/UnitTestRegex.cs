583b0e3835596a87f2e27bd82d48b8aa48e556a9
kaeaddaar <playinfc@hotmail.ca>
2018-10-08 15:42:13 -0700

    Added Regex for finding text surrounded by brackets

--- SomeDemos/TestRegex/UnitTestRegex.cs
+++ SomeDemos/TestRegex/UnitTestRegex.cs
@@ -17,7 +17,21 @@ namespace TestRegex
 250-48507712"; // this line isn't a phone number due to the 7 characters instead of 3 then 4
             MatchCollection Col = RegexExpressions.GetPhoneNumbers(ToTest);
             Assert.AreEqual(5, Col.Count);
+            Assert.AreEqual("+250 486-7712", Col[0].Value);
+            Assert.AreEqual("+250-486-7712", Col[1].Value);
+            Assert.AreEqual("250-486-7712", Col[2].Value);
+            Assert.AreEqual("250 486 7712", Col[3].Value);
+            Assert.AreEqual("+250-486-7712", Col[4].Value);
+        }
 
+        [TestMethod]
+        public void TestFindTextInBrackets()
+        {
+            string ToTest = @"abc123(hello), asdlf;kj(world)";
+            MatchCollection Col = RegexExpressions.GetTextFromBracketToBracket(ToTest);
+            Assert.AreEqual(2, Col.Count);
+            Assert.AreEqual("(hello)", Col[0].Value);
+            Assert.AreEqual("(world)", Col[1].Value);
         }
     }
 }
--- SomeDemos/libRegex/RegexExpressions.cs
+++ SomeDemos/libRegex/RegexExpressions.cs
@@ -9,11 +9,20 @@ namespace libRegex
         // /(\+?[0-9]{3})(-| )([0-9]{3})(-| )([0-9]{4})/g    note that the opening / and following /g are not needed
         //const string PhoneNumberExpression = "/" + @"(\+?[0-9]{3})" + @"( |-)" + @"([0-9]{3})" + @"( |-)" + @"([0-9]{4})" + "/g";
         const string PhoneNumberExpression = @"(\+?[0-9]{3})" + @"( |-)" + @"([0-9]{3})" + @"( |-)" + @"([0-9]{4})";
+        const string BracketContentExpression = @"([(])(\w)+([)])";
+
         public static MatchCollection GetPhoneNumbers(string MayContainPhoneNumbers)
         {
             // can I check to see if the expression validates?
             Regex R = new Regex(PhoneNumberExpression);
             MatchCollection M = R.Matches(MayContainPhoneNumbers);
+            return M;
+        }
+
+        public static MatchCollection GetTextFromBracketToBracket(string MayContainBracketPairs)
+        {
+            Regex R = new Regex(BracketContentExpression);
+            MatchCollection M = R.Matches(MayContainBracketPairs);
             return M;
         }
     }
